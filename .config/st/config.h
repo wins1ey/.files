fc1a3370884229afa17a6ea19840ff52b0ef984f
wins1ey <wins1ey@wins1ey.win>
2024-04-15 23:10:14 +0100

    Update st to use default shell

--- .config/st/config.h
+++ .config/st/config.h
@@ -16,7 +16,7 @@ static int borderpx = 10;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/bin/zsh";
+static char *shell = "/bin/sh";
 char *utmp = NULL;
 /* scroll program: to enable use a string like "scroll" */
 char *scroll = NULL;
